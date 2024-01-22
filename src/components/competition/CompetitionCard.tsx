d3f676d51be3d1c95f14809a62d6be17bdb3b49a
Zaid Arshad (Nico) <nico.03727+github@gmail.com>
2024-01-22 01:29:54 -0500

    feat: competitions

---
+++ src/components/competition/CompetitionCard.tsx
@@ -0,0 +1,34 @@
+import { Image } from "astro:assets";
+import Reef from "../../imgs/reef.png";
+
+export interface CompetitionProps {
+    name: string;
+    placement: number | string;
+    track?: string;
+    date: string;
+}
+
+export default function CompetitionCard(props: CompetitionProps) {
+    return <div className="flex flex-col items-center gap-2 w-48 h-56 py-4 px-2 border-2 border-slate-800 rounded-xl">
+        <div className="relative w-44 h-44">
+            <img
+                alt="Placement reef"
+                className="absolute object-cover top-0 left-0 -z-1 w-full h-full"
+                src={Reef.src}
+                width={200}
+                height={200}
+            />
+            <p
+                className={`absolute ${typeof props.placement === "string" ? "text-2xl w-[1rem] top-[35%] left-[35%]" : "text-6xl top-[25%] left-[40%] "} font-bold`}
+            >
+                {props.placement}
+            </p>
+        </div>
+
+        <div className="flex flex-col items-center">
+            <p className="text-lg">{props.name}</p>
+            {props.track && <p className="text-sm text-gray-500">{props.track}</p>}
+            <p className="text-xs text-gray-500">{props.date}</p>
+        </div>
+    </div>
+}
---
+++ src/data/competitions.ts
@@ -0,0 +1,26 @@
+export const competitions_data = [
+	{
+		name: "HackPrinceton",
+		placement: 1,
+		track: "Education Track",
+		date: "4/2/23",
+	},
+	{
+		name: "Wix Create-A-SaaS",
+		placement: "MVP",
+		track: "SaaS Track",
+		date: "4/2/23",
+	},
+	{
+		name: "Buffalo State CS4HS",
+		placement: 3,
+		track: "Game Dev Track",
+		date: "4/2/23",
+	},
+	{
+		name: "Buffalo State CS4HS",
+		placement: 2,
+		track: "Web Design Track",
+		date: "4/2/23",
+	},
+];
