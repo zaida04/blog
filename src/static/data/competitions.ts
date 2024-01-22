b48ff6f7ca905b0a33c448cc1ad984056365e617
Zaid Arshad (Nico) <nico.03727+github@gmail.com>
2024-01-22 13:59:58 -0500

    chore: update competitions

--- src/static/data/competitions.ts
+++ src/static/data/competitions.ts
@@ -9,18 +9,18 @@ export const competitions_data = [
 		name: "Wix Create-A-SaaS",
 		placement: "MVP",
 		track: "SaaS Track",
-		date: "4/2/23",
+		date: "7/22/2022",
 	},
 	{
 		name: "Buffalo State CS4HS",
 		placement: 3,
 		track: "Game Dev Track",
-		date: "4/2/23",
+		date: "7/26/15",
 	},
 	{
 		name: "Buffalo State CS4HS",
 		placement: 2,
 		track: "Web Design Track",
-		date: "4/2/23",
+		date: "7/26/15",
 	},
 ];
