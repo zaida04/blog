d2d14cd3cd3e7b21a2708156108a4685d10649ea
zaida04 <nico.03727+github@gmail.com>
2024-06-06 21:37:38 -0400

    chore: add vercel build adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import vercel from '@astrojs/vercel/serverless';
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), react(), mdx()]
+  output: 'server',
+  integrations: [
+    tailwind({
+      applyBaseStyles: false
+    }),
+    react(),
+    mdx(),
+    vercel()
+  ]
 });
