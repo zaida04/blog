79bca44e1f22d77d839ceaf76cc01dc45ed03210
Nico <nico.03727+github@gmail.com>
2024-07-15 21:41:18 -0400

    chore: update competitions list to be date accurate

--- src/content/data/competitions.ts
+++ src/content/data/competitions.ts
@@ -1,15 +1,15 @@
 export const competitions_data = [
 	{
+		name: "Wix Create-A-SaaS",
+		placement: "MVP",
+		track: "SaaS Track",
+		date: "5/02/23",
+	},
+	{
 		name: "HackPrinceton",
 		placement: 1,
 		track: "Education Track",
 		date: "4/2/23",
-	},
-	{
-		name: "Wix Create-A-SaaS",
-		placement: "MVP",
-		track: "SaaS Track",
-		date: "7/22/22",
 	},
 	{
 		name: "Buffalo State CS4HS",
